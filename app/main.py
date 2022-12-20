866b8b160a4c1f871612ca6e8de7ac8c428ac003
jessegabriels <r0882112@student.thomasmore.be>
2022-12-20 10:48:29 +0100

    cors

--- app/main.py
+++ app/main.py
@@ -33,9 +33,9 @@ origins = [
     "http://localhost:8080",
     "https://localhost.tiangolo.com",
     "http://127.0.0.1:5500",
-    "https://jessegabriels.github.io/*"
-    #"https://jessegabriels.github.io/API-Project2-Front/*",
-    #"https://jessegabriels.github.io/API-Project2-Front/index.html"
+    "https://jessegabriels.github.io/*",
+    "https://jessegabriels.github.io/API-Project2-Front/*",
+    "https://jessegabriels.github.io/API-Project2-Front/index.html"
 ]
 
 project.add_middleware(
